512f7b28eebff4902e951041d869601e4bd3a85c
KunalSher <kunalsharma.jpr10@gmail.com>
2026-07-07 23:22:53 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (78.84%) - LeetSync

---
+++ 75-sort-colors/sort-colors.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int cntred=0;
+        int cntwhite=0;
+        int cntblue=0;
+        int limit=nums.size();
+        nums.clear();
+
+        for (int i =0; i < limit; i ++){
+            if (nums[i]==0){
+                cntred++;
+
+            }
+
+            else if (nums[i]==1){
+                cntwhite++;
+            }
+
+            else {
+                cntblue++;
+            }
+        }
+            
+                for (int i =0; i < limit; i ++){
+                    if (cntred!=0){
+                        nums.push_back(0);
+                        cntred--;
+                    }
+                    else if (cntred==0 && cntwhite!=0){
+                        nums.push_back(1);
+                        cntwhite--;
+
+                    }
+                    else if (cntred==0 && cntwhite==0 && cntblue!=0) {
+                        nums.push_back(2);
+                        cntblue--;
+                    }
+                }
+    }
+
+
+};
